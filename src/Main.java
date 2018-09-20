47cf5b6b78daca1910fbd3fc18b34b61ee6072e4
EmilDino <EmilLD123@hotmail.com>
2018-09-20 13:45:01 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // opgave 1
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java now");
+        System.out.println("Programming is fun");
+
+        //opgave 1.2
+        for(int x = 1; x <=5; x++) {
+            System.out.println("i love Java");
+        }
+
+        // opgave 1.5
+        System.out.println((7.5*6.5-4.5*3)/(47.5-5.5));
+        // opgave 1.6
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+
+    }
+}
